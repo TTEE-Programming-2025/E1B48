7b8d3ce48582469ffdc2e5ac5346c7c06df8aef5
DorrisEB <milkp1116@gmail.com>
2025-04-02 01:14:54 +0800

    個人風格介面

---
+++ E1B48.c
@@ -0,0 +1,35 @@
+#include <stdio.h> 
+#include <stdlib.h>
+
+int main()
+{
+	//Part1:個人風格畫面 
+	printf("========================================\n");
+	printf("|                                      |\n");
+	printf("|                                      |\n");
+	printf("|        o(≧▽≦)o歡迎 o(≧▽≦)o     |\n");
+	printf("|                                      |\n");
+	printf("|                                      |\n");
+	printf("|                                      |\n");
+	printf("|                 E1B48                |\n");
+	printf("|                黃郁婷                |\n");
+	printf("|                                      |\n");
+	printf("|                                      |\n");
+	printf("|                                      |\n");
+	printf("|             (╯°□°）╯︵              |\n");
+	printf("|                                      |\n");
+	printf("|                                      |\n");
+	printf("|                                      |\n");
+	printf("|         o(≧▽≦)o  o(≧▽≦)o       |\n");
+	printf("|                                      |\n");
+	printf("|            感恩老師感恩學長          |\n");
+	printf("|            拜託不要當我 QAQ          |\n");
+	printf("========================================\n");
+	
+	system("pause");
+	system("cls");
+
+	system("pause");
+	return 0;
+}
+
