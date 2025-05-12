eb51e29bcc43fa0eb72bca25f5c45b2783660bb5
DorrisEB <milkp1116@gmail.com>
2025-05-13 00:21:26 +0800

    個人風格畫面和密碼

---
+++ HW3E1B48.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+	//Part1:個人風格畫面 &密碼驗證 
+	printf("========================================\n");
+	printf("|                                      |\n");
+	printf("|                                      |\n");
+	printf("|        o(≧▽≦)o歡迎 o(≧▽≦)o     |\n");
+	printf("|                                      |\n");
+	printf("|                                      |\n");
+	printf("|                                      |\n");
+	printf("|                 E1B48                |\n");
+	printf("|                黃郁婷                |\n");
+	printf("|                                      |\n");
+	printf("|                                      |\n");
+	printf("|                                      |\n");
+	printf("|             (╯°□°）╯︵              |\n");
+	printf("|                                      |\n");
+	printf("|                                      |\n");
+	printf("|                                      |\n");
+	printf("|         o(≧▽≦)o  o(≧▽≦)o       |\n");
+	printf("|                                      |\n");
+	printf("|                                      |\n");
+	printf("|                                      |\n");
+	printf("========================================\n");
+	
+	system("pause");
+	system("cls");
+	
+	int password,correctpassword=2025,wrong=0;
+	do
+	{
+		printf("請輸入4位數字密碼\n");
+		scanf("%d",&password);
+	
+		if(password==correctpassword)
+		{
+			printf("歡迎！\n");
+			system("pause");
+			system("cls");
+			break;
+		}
+		else
+		{
+			printf("%c",'\a');
+			printf("密碼錯誤！");
+			wrong++;
+			if(wrong==3)
+			{
+			printf("密碼錯誤3次！結束程式！");
+			return 1;
+			}
+		}
+	}while(1);
+} 
